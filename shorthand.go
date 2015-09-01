b9e5581bc15bd4acb7da3587fc9ebf067a7741c7
R. S. Doiel <rsdoiel@gmail.com>
2015-08-31 17:49:50 -0700

    Started to rewrite shorthand.go to be more robust to reliably support :{ and :={

--- shorthand.go
+++ shorthand.go
@@ -12,14 +12,14 @@
 package shorthand
 
 import (
-	"errors"
 	"fmt"
-	"github.com/russross/blackfriday"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/russross/blackfriday"
 )
 
 // The version nummber of library and utility
@@ -56,12 +56,17 @@ var ops = []string{
 
 // SourceMap holds the source and value of an assignment
 type SourceMap struct {
-	src   string
-	value string
+	Label    string // Label is the symbol to be replace based on Op and Value
+	Op       string // Op is the type of assignment being made (if is an empty string if not an assignment)
+	Value    string // Value is argument to the right of Op
+	Expanded string // Expanded is the value calculated based on Label, Op and Value
+	LineNo   int
 }
 
-// Abbrevations holds the shorthand and translation
-var Abbreviations = make(map[string]SourceMap)
+// SymbolTable holds the exressions, values and other errata of parsing assignments making expansions
+type SymbolTable struct {
+	entries []SourceMap
+}
 
 // IsAssignment checks to see if a string contains an assignment (e.g. has a ' := ' in the string.)
 func IsAssignment(text string) bool {
@@ -74,20 +79,25 @@ func IsAssignment(text string) bool {
 }
 
 // HasAssignment checks to see if a shortcut has already been assigned.
-func HasAssignment(key string) bool {
-	_, ok := Abbreviations[key]
-	return ok
+func HasAssignment(table SymbolTable, label string) bool {
+	for _, sm := range table.entries {
+		if sm.Label == label {
+			return true
+		}
+
+	}
+	return false
 }
 
-// ParseDefinition returns LABEL, ASSIGNMENT, VALUE
-func ParseDefinition(s string) (string, string, string) {
+// Parse returns SourceMap object and bool with true if assignment or false if not
+func Parse(s string, lineNo int) (SourceMap, bool) {
 	for _, op := range ops {
 		if strings.Index(s, op) != -1 {
 			parts := strings.SplitN(strings.TrimSpace(s), op, 2)
-			return parts[0], op, parts[1]
+			return SourceMap{Label: parts[0], Op: op, Value: parts[1], LineNo: lineNo, Expanded: ""}, true
 		}
 	}
-	return "", "", ""
+	return SourceMap{Label: "", Op: "", Value: "", LineNo: lineNo, Expanded: s}, false
 }
 
 // WriteAssignment writes a single assignment statement to filename
@@ -99,27 +109,26 @@ func WriteAssignment(fname string, sm SourceMap, writeSourceCode bool) bool {
 	defer fp.Close()
 
 	if writeSourceCode == true {
-		fmt.Fprintf(fp, "%s", sm.src)
+		fmt.Fprintf(fp, "%s%s%s", sm.Label, sm.Op, sm.Value)
 	} else {
-		fmt.Fprintf(fp, "%s", sm.value)
+		fmt.Fprintf(fp, "%s", sm.Expanded)
 	}
 	return true
 }
 
 // WriteAssignments write all assignment statements to filename
-func WriteAssignments(fname string, assigned map[string]SourceMap, writeSourceCode bool) bool {
+func WriteAssignments(fname string, table SymbolTable, writeSourceCode bool) bool {
 	fp, err := os.Create(fname)
 	if err != nil {
 		log.Printf("Cannot write to %s, error: %s\n", fname, err)
 		return false
 	}
 	defer fp.Close()
-	for k := range assigned {
-		//FIXME: Probably should not write out the value of _
+	for _, sm := range table.entries {
 		if writeSourceCode == true {
-			fmt.Fprintf(fp, "%s\n", Abbreviations[k].src)
+			fmt.Fprintf(fp, "%s%s%s\n", sm.Label, sm.Op, sm.Value)
 		} else {
-			fmt.Fprintf(fp, "%s\n", Abbreviations[k].value)
+			fmt.Fprintf(fp, "%s\n", sm.Expanded)
 		}
 	}
 	return true
@@ -128,25 +137,20 @@ func WriteAssignments(fname string, assigned map[string]SourceMap, writeSourceCo
 // ReadAssignments read in all of the lines of fname and
 // add any assignment statements found to Abbreviations
 // and expand any assignments and return as a string.
-func ReadAssignments(fname string) (string, error) {
-	var (
-		out []string
-	)
+func ReadAssignments(fname string, table SymbolTable) error {
 	buf, err := ioutil.ReadFile(fname)
 	if err != nil {
-		return "", errors.New(fmt.Sprintf("Cannot read %s: %s\n", fname, err))
+		return fmt.Errorf("Cannot read %s: %s\n", fname, err)
 	}
-	for i, line := range strings.Split(string(buf), "\n") {
-		if IsAssignment(line) {
-			ok := Assign(line)
+	for i, text := range strings.Split(string(buf), "\n") {
+		if IsAssignment(text) {
+			ok := Assign(table, text, i)
 			if ok == false {
-				return "", errors.New(fmt.Sprintf("Error at line %d in %s\n", i+1, fname))
+				return fmt.Errorf("Error at line %d in %s\n", i+1, fname)
 			}
-		} else {
-			out = append(out, Expand(line))
 		}
 	}
-	return strings.Join(out, "\n"), nil
+	return nil
 }
 
 // ReadMarkdown reads in a file and processes it with Blackfriday MarkdownCommon
@@ -158,77 +162,66 @@ func ReadMarkdown(fname string) string {
 	return string(blackfriday.MarkdownCommon(buf))
 }
 
-// Assign stores a shorthand and its expansion or writes and assignment or assignments
-func Assign(s string) bool {
-	var (
-		key   string
-		op    string
-		value string
-	)
-
-	if IsAssignment(s) {
-		key, op, value = ParseDefinition(s)
-	} else {
-		log.Fatalf("[%s] is an invalid assignment.\n", s)
-	}
-
-	if op == OutputAssignedValue {
-		return WriteAssignment(value, Abbreviations[key], false)
-	} else if op == OutputAssignedValues {
-		return WriteAssignments(value, Abbreviations, false)
-	} else if op == OutputAssignment {
-		return WriteAssignment(value, Abbreviations[key], true)
-	} else if op == OutputAssignments {
-		return WriteAssignments(value, Abbreviations, true)
-	} else if op == AssignExpansion {
-		value = Expand(value)
-	} else if op == IncludeAssignments {
-		value, err := ReadAssignments(value)
-		if err != nil {
-			log.Fatalf("Error processing %s: %s\n", value, err)
-		}
-	} else if op == AssignInclude {
-		buf, err := ioutil.ReadFile(value)
-		if err != nil {
-			log.Fatalf("Cannot read %s: %v\n", value, err)
-		}
-		value = string(buf)
-	} else if op == AssignShell {
-		buf, err := exec.Command("bash", "-c", value).Output()
-		if err != nil {
-			log.Fatalf("Shell command returned error: %s\n", err)
-		}
-		value = string(buf)
-	} else if op == AssignMarkdown {
-		value = strings.TrimSpace(string(blackfriday.MarkdownCommon([]byte(value))))
-	} else if op == IncludeMarkdown {
-		value = ReadMarkdown(value)
-	}
-
-	if op == "" || key == "" || value == "" {
-		return false
-	}
-	if key == "_" {
-		return true
-	}
-	Abbreviations[key] = SourceMap{src: s, value: value}
-	_, ok := Abbreviations[key]
-	return ok
-}
-
-// Expand takes a text and expands all shorthands
-func Expand(text string) string {
+// Expand takes some text and expands all labels to their values
+func Expand(table SymbolTable, text string) string {
 	// Iterate through the list of key/SourceMaps in abbreviations
-	for key, sm := range Abbreviations {
-		text = strings.Replace(text, key, sm.value, -1)
+	for _, sm := range table.entries {
+		text = strings.Replace(text, sm.Label, sm.Expanded, -1)
 	}
-	//fmt.Printf("DEBUG Expand(%s)\n", text)
 	return text
 }
 
-// Clear remove all the elements of a map.
-func Clear() {
-	for key := range Abbreviations {
-		delete(Abbreviations, key)
+// Assign stores a shorthand and its expansion or writes and assignment or assignments
+func Assign(table SymbolTable, s string, lineNo int) bool {
+	sm, ok := Parse(s, lineNo)
+	if ok == false {
+		return ok
 	}
+
+	// These functions do not change the symbol table
+	if sm.Op == OutputAssignedValue {
+		return WriteAssignment(sm.Value, sm, false)
+	}
+
+	if sm.Op == OutputAssignedValues {
+		return WriteAssignments(sm.Value, table, false)
+	}
+
+	if sm.Op == OutputAssignment {
+		return WriteAssignment(sm.Value, sm, true)
+	}
+
+	if sm.Op == OutputAssignments {
+		return WriteAssignments(sm.Value, table, true)
+	}
+
+	// These functions change the symbol table
+	if sm.Op == AssignExpansion {
+		sm.Expanded = Expand(table, sm.Value)
+	} else if sm.Op == IncludeAssignments {
+		err := ReadAssignments(sm.Value, table)
+		if err != nil {
+			log.Fatalf("Error processing %s: %s\n", sm.Value, err)
+		}
+	} else if sm.Op == AssignInclude {
+		buf, err := ioutil.ReadFile(sm.Value)
+		if err != nil {
+			log.Fatalf("Cannot read %s: %s\n", sm.Value, err)
+		}
+		sm.Expanded = string(buf)
+	} else if sm.Op == AssignShell {
+		buf, err := exec.Command("bash", "-c", sm.Value).Output()
+		if err != nil {
+			log.Fatalf("Shell command returned error: %s\n", err)
+		}
+		sm.Expanded = string(buf)
+	} else if sm.Op == AssignMarkdown {
+		sm.Expanded = strings.TrimSpace(string(blackfriday.MarkdownCommon([]byte(sm.Value))))
+	} else if sm.Op == IncludeMarkdown {
+		sm.Expanded = ReadMarkdown(sm.Value)
+	}
+	if sm.Label != "" {
+		table.entries = append(table.entries, sm)
+	}
+	return ok
 }
