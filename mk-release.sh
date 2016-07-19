abe2e76dee994d6f9087550893a6807103d529a0
R. S. Doiel <rsdoiel@gmail.com>
2016-07-18 17:24:42 -0700

    snapshot

--- mk-release.sh
+++ mk-release.sh
@@ -2,6 +2,7 @@
 #
 # Make releases for Linux/amd64, Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
+REPONAME=shorthand
 for PROGNAME in shorthand; do
     env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env GOOS=darwin	GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
@@ -9,3 +10,5 @@ for PROGNAME in shorthand; do
     env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 done
+
+zip -r $REPONAME-compiled.zip README.md INSTALL LICENSE dist/*
