381724e4339c9e12b7be63b5183afae800eb9d1d
R. S. Doiel <rsdoiel@gmail.com>
2016-12-24 13:19:25 -0800

    Quick Save

--- Makefile
+++ Makefile
@@ -6,6 +6,12 @@
 # Released under the BSD 2-Clause license
 # See: http://opensource.org/licenses/BSD-2-Clause
 #
+PROJECT = shorthand
+
+VERSION = $(shell grep -m1 'Version = ' $(PROJECT).go | cut -d\"  -f 2)
+
+BRANCH = $(shell git branch | cut -d\  -f 2)
+
 build:
 	go build -o bin/shorthand cmds/shorthand/shorthand.go
 	shorthand build.shorthand
@@ -18,10 +24,14 @@ lint:
 test:
 	go test
 
+save:
+	git commit -am "Quick Save"
+	git push origin $(BRANCH)
+
 clean:
 	if [ -d bin ]; then /bin/rm -fR bin; fi
 	if [ -d dist ]; then /bin/rm -fR dist; fi
-	if [ -f shorthand-binary-release.zip ]; then /bin/rm shorthand-binary-release.zip; fi
+	if [ -f $(PROJECT)-$(VERSION)-release.zip ]; then /bin/rm $(PROJECT)-$(VERSION)-release.zip; fi
 
 install:
 	GOBIN=$(HOME)/bin go install cmds/shorthand/shorthand.go
--- mk-release.sh
+++ mk-release.sh
@@ -2,8 +2,14 @@
 #
 # Make releases for Linux/amd64, Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
-RELEASE_NAME=shorthand
+PROJECT=shorthand
+
+VERSION=$(grep -m1 'Version = ' $PROJECT.go | cut -d\"  -f 2)
+
+RELEASE_NAME=$PROJECT-$VERSION
+echo "Preparing $RELEASE_NAME"
 for PROGNAME in shorthand; do
+    echo "Cross compiling $PROGNAME"
     env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env GOOS=darwin	GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
@@ -11,4 +17,9 @@ for PROGNAME in shorthand; do
     env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 done
 
-zip -r $RELEASE_NAME-binary-release.zip README.md LICENSE INSTALL.md dist/*
+echo "Assembling dist/"
+for FNAME in README.md LICENSE INSTALL.md; do
+    cp -v $FNAME dist/
+done
+echo "Zipping $RELEASE_NAME"
+zip -r $RELEASE_NAME-release.zip dist/*
